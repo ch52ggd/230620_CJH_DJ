f875f7bf3485d717a98732315c90e99bebb9df20
ch52ggd <136698331+ch52ggd@users.noreply.github.com>
2023-06-20 21:02:37 +0900

    과정 (1)
    과정

--- 230620_CJH_DJ/Assets/PlayerMove.cs
+++ 230620_CJH_DJ/Assets/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour
 {
     public GameObject player;
 
+    bool isChange = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,16 +17,28 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isChange)
+        {
+            if(player.transform.position.x >= 2)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
     }
 
     public void LeftMove()
     {
-
+        isChange = false;
+        player.transform.position -= new Vector3(10 * Time.deltaTime, 0, 0);
     }
 
     public void RightMove()
     {
-
+        isChange = true;
+        player.transform.position += new Vector3(10 * Time.deltaTime, 0, 0); 
     }
 }
